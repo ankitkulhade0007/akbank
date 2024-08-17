90bc61d109d649be14b5ef3cb858b83e4d473df9
ankitkulhade0007 <ankitkulhade123@gmail.com>
2024-08-18 01:57:08 +0530

    Method level security. @Pre and @Post

--- src/main/java/com/akBank/akbank/controller/LoansController.java
+++ src/main/java/com/akBank/akbank/controller/LoansController.java
@@ -4,6 +4,7 @@ import com.akBank.akbank.model.Loans;
 import com.akBank.akbank.repository.LoanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class LoansController {
     private LoanRepository loanRepository;
 
     @GetMapping("/myLoans")
+    @PostAuthorize("hasRole('USER')")
     public List<Loans> getLoanDetails(@RequestParam long id) {
         List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
         if (loans != null) {
