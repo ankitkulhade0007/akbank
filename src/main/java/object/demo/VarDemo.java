8ccfafe04d4f26659448b49bd663d7eb4f6d200d
ankitkulhade0007 <ankitkulhade123@gmail.com>
2024-09-13 02:32:12 +0530

    Pojo

---
+++ src/main/java/object/demo/VarDemo.java
@@ -0,0 +1,11 @@
+package object.demo;
+
+public class VarDemo {
+
+    public static void main(String[] args) {
+        var message = "Hello World !";
+        var person = new Person();
+        var arr = new ArrayIndexOutOfBoundsException();
+    }
+
+}
